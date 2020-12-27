70dd8462251c792fb278519cffe5433e43cdb549
Mark Birger <birgerm@yandex.ru>
2020-12-27 11:26:55 +0200

    Bump version to 0.2.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ PROJECT_ROOT, _ = os.path.split(__file__)
 NAME = "kafka-logging-handler"
 EMAILS = "mbirger@redhat.com, mjahudko@readhat.com, rmonegro@redhat.com"
 AUTHORS = "Mark Birger, Michaela Jahudkova, Robert Monegro"
-VERSION = "0.2.4"
+VERSION = "0.2.5"
 
 URL = "https://github.com/redhat-aqe/kafka-logging-handler"
 LICENSE = "GPLv3"
